d476845c1c173f024ba1898f6604434f1b1e686d
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2023-08-12 22:10:49 -0300

    Update aula10a.py

--- pacote-download/ExercíciosDePython/aula10a.py
+++ pacote-download/ExercíciosDePython/aula10a.py
@@ -2,6 +2,6 @@ n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digite a segunda nota: '))
 m = (n1 + n2)/2
 if m <= 7.0:
-    print('Se fudeu! Sua média final foi {}'.format(m))
+    print('Se deu mal! Sua média final foi {}'.format(m))
 else:
     print('Boa mlk! Sua média final foi {}'.format(m))
